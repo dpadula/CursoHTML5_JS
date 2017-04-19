1da8c8565a219577d20fd8b72c3b6ce0a5fa9649
dpadula <diegopadula@gmail.com>
2017-04-19 12:06:51 -0300

    Agregado chequeo paridad y estilos acorde a este valor

--- SolucionLab04/app.js
+++ SolucionLab04/app.js
@@ -1,10 +1,18 @@
 angular.module("labo04app", [])
     .controller("labo04Controller",
         function ($scope) {
-            $scope.total = 2 + 1;            
+            $scope.total = 2 + 1;
+            $scope.valorActual = Math.floor($scope.total / 2);
+            $scope.paridad = ($scope.valorActual % 2) == 0;
+            $scope.textoParidad = $scope.paridad ? ' - PAR' : ' - IMPAR';
+            console.log($scope.paridad, $scope.textoParidad);
             $scope.mitadValor = $scope.total / 2;
             $scope.refresh = function () {
                 $scope.total += Math.random();
+                $scope.valorActual = Math.floor($scope.total / 2);
+                $scope.paridad = ($scope.valorActual % 2) == 0;
+                $scope.textoParidad = $scope.paridad ? ' - PAR' : ' - IMPAR';
+                console.log($scope.paridad, $scope.textoParidad);
                 $scope.mitadValor = $scope.total / 2;
             }
         }
