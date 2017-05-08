232b7886c6b5a785cf1fdf689559f4a1e1537c79
dpadula <diegopadula@gmail.com>
2017-05-08 13:12:07 -0300

    Agregados

--- Tutorials/MEAN_Steps/Proyecto/contactlistapp/public/controllers/controller.js
+++ Tutorials/MEAN_Steps/Proyecto/contactlistapp/public/controllers/controller.js
@@ -1,4 +1,32 @@
 var myApp = angular.module('myContactListApp', []);
-myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
     console.log("Hello World from controller");
+
+    $http.get('/contactlist').then(function(response){
+        console.log("Recibi los datos");
+        console.log(response.data);
+        $scope.contactlist = response.data;
+    });
+
+    //Esto se movio al servidor (server.js) y se pide a traves de una solicitud GET que esta arriba.
+    // person1 = {
+    //     name: "Tim",
+    //     email: "tim@email.com",
+    //     phone: "111-111-111"
+    // };
+
+    // person2 = {
+    //     name: "Emily",
+    //     email: "emy@email.com",
+    //     phone: "222-222-222"
+    // };
+
+    // person3 = {
+    //     name: "John",
+    //     email: "john@email.com",
+    //     phone: "333-333-333"
+    // };
+
+    // contactlist = {person1, person2, person3};
+    // $scope.contactlist = contactlist;
 }]);
--- Tutorials/MEAN_Steps/Proyecto/contactlistapp/server.js
+++ Tutorials/MEAN_Steps/Proyecto/contactlistapp/server.js
@@ -8,6 +8,31 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactlist', function(req, res){
+    console.log("Recibi una solicitud GET");
+    // res.send("Hola desde servidor Express");  
+    person1 = {
+        name: "Tim",
+        email: "tim@email.com",
+        phone: "111-111-111"
+    };
+
+    person2 = {
+        name: "Emily",
+        email: "emy@email.com",
+        phone: "222-222-222"
+    };
+
+    person3 = {
+        name: "John",
+        email: "john@email.com",
+        phone: "333-333-333"
+    };
+
+    contactlist = {person1, person2, person3};
+    res.json(contactlist);
+});
+
 var portNumber = 3000;
 app.listen(portNumber);
 console.log("Server levantado escuchando en puerto: " + portNumber);
