d885f1a74decc0baa2b1214bc56ee44afe286ffd
dpadula <diegopadula@gmail.com>
2017-05-24 10:41:55 -0300

    Actualizsacion del metodo borrar

--- SolucionLab05/app/app.js
+++ SolucionLab05/app/app.js
@@ -4,7 +4,7 @@ controller("areaController", function ($scope) {
     $scope.listaAreas = [];
     $scope.area = undefined;
     //$scope.deshabilitarGuardar = true;
-    $scope.deshabilitarInput = true;    
+    $scope.deshabilitarInput = true;
 
     // resto del controlador con funciones de control de la vista
     $scope.cancelar = function () {
@@ -17,17 +17,17 @@ controller("areaController", function ($scope) {
     }
     $scope.guardar = function () {
         console.log($scope.area);
-        console.log($scope.area.nombre);//Este nombre viene de ng-model declarativo en HTML
-        console.log($scope.listaAreas);            
+        console.log($scope.area.nombre); //Este nombre viene de ng-model declarativo en HTML
+        console.log($scope.listaAreas);
         if ($scope.area && $scope.area.nombre) {
             $scope.listaAreas.push($scope.area);
             $scope.area = undefined;
             //$scope.deshabilitarGuardar = (typeof $scope.area === 'undefined' ? true : false);
-            $scope.deshabilitarInput = true;            
+            $scope.deshabilitarInput = true;
         } else {
             $scope.mensaje = "Hubo un problema, el área no puede tener nombre nulo";
             console.log($scope.mensaje);
-        }        
+        }
     }
     $scope.nueva = function () {
         // asigna a $scope.area un objeto vacio 
@@ -41,6 +41,19 @@ controller("areaController", function ($scope) {
         // implementa el método findIndex para encontra el indice para un objeto que tenga el atributo nombre 
         // con el mismo nombre del área recibida como parámetro. Cuando lo encuentra lo utiliza en el arreglo con 
         // el método splice para eliminar un elemento
-        
+        console.log($scope.listaAreas);
+
+        var i = $scope.listaAreas.findIndex(a => {
+            // console.log(a.nombre);
+            return (a.nombre === unArea.nombre);
+        });
+        console.log("Elemento es: " + unArea.nombre + " Con indice: " + i);
+        var borrar = confirm("Esta a punto de borrar el elemento: " + unArea.nombre + " Con indice: " + i + " Está seguro?");
+        if (borrar) {
+            $scope.listaAreas.splice(i, 1);
+            console.log("Se borró el elemento: " + unArea.nombre + " Con indice: " + i);
+        } else {
+            console.log("NO se borró el elemento: " + unArea.nombre + " Con indice: " + i);
+        }
     }
 });
