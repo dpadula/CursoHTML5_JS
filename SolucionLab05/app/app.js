b12e06108a7bcccc3530538becaa35bd5fe79e95
dpadula <diegopadula@gmail.com>
2017-05-24 09:41:37 -0300

    Actualizacion TP4

--- SolucionLab05/app/app.js
+++ SolucionLab05/app/app.js
@@ -3,6 +3,8 @@ controller("areaController", function ($scope) {
     $scope.titulo = "Gestión de Areas";
     $scope.listaAreas = [];
     $scope.area = undefined;
+    //$scope.deshabilitarGuardar = true;
+    $scope.deshabilitarInput = true;    
 
     // resto del controlador con funciones de control de la vista
     $scope.cancelar = function () {
@@ -14,24 +16,31 @@ controller("areaController", function ($scope) {
         console.log($scope.area);
     }
     $scope.guardar = function () {
+        console.log($scope.area);
+        console.log($scope.area.nombre);//Este nombre viene de ng-model declarativo en HTML
+        console.log($scope.listaAreas);            
         if ($scope.area && $scope.area.nombre) {
             $scope.listaAreas.push($scope.area);
-            console.log($scope.area);
-            console.log($scope.area.nombre);//Este nombre viene de ng-model declarativo en HTML
-            console.log($scope.listaAreas);
+            $scope.area = undefined;
+            //$scope.deshabilitarGuardar = (typeof $scope.area === 'undefined' ? true : false);
+            $scope.deshabilitarInput = true;            
         } else {
             $scope.mensaje = "Hubo un problema, el área no puede tener nombre nulo";
             console.log($scope.mensaje);
-        }
+        }        
     }
     $scope.nueva = function () {
         // asigna a $scope.area un objeto vacio 
         $scope.area = {};
         console.log($scope.area);
+        //$scope.deshabilitarGuardar = (((typeof $scope.area === 'undefined') || !$scope.area.nombre) ? true : false);
+        $scope.deshabilitarInput = false;
+        console.log($scope.deshabilitarGuardar);
     }
     $scope.borrar = function (unArea) {
         // implementa el método findIndex para encontra el indice para un objeto que tenga el atributo nombre 
         // con el mismo nombre del área recibida como parámetro. Cuando lo encuentra lo utiliza en el arreglo con 
         // el método splice para eliminar un elemento
+        
     }
 });
